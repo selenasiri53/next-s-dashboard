536e63099673420a7519f3ba7d8914e6142a4474
selenasiri53 <selenasiri53@gmail.com>
2024-01-12 19:11:55 -0500

    update invoices table

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,10 +1,21 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useSearchParams, usePathname, useRouter } from 'next/navigation'; // 2. update URL with search params - usePathname, useRouter
 
-export default function Search({ placeholder }: { placeholder: string }) {
+export default function Search({ placeholder }: { placeholder: string }) { // 1. capture user's input
+  const searchParams = useSearchParams();
+  const pathname = usePathname(); // update URL ...
+  const { replace } = useRouter(); // update URL ...
+
   function handleSearch(term: string) {
-    console.log(term);
+    const params = new URLSearchParams(searchParams)
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+    replace(`${pathname}?${params.toString()}`); // update URL ...
   }
 
   return (
@@ -16,8 +27,9 @@ export default function Search({ placeholder }: { placeholder: string }) {
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
         onChange = {(e) => {
-          handleSearch(e.target.value);
+          handleSearch(e.target.value); // 1. capture user's input -- onChange function
         }}
+        defaultValue={searchParams.get('query')?.toString()} // 3. keeping URL and input value in sync
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
