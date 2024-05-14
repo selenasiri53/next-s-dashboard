9f1400af84da806baf6041b4badb048211ebabbe
Selena Siri <selenasiri53@gmail.com>
2024-05-14 15:47:22 -0400

    added login as <h2>

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -8,6 +8,8 @@ export default function LoginPage() {
         <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
           <div className="w-32 text-white md:w-36">
             <AcmeLogo />
+            <h2>Username: user@nextmail.com</h2>
+            <h2>Password: 123456</h2>
           </div>
         </div>
         <LoginForm />
